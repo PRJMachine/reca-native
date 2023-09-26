eca4a2ba6def4b450b21b34e1dc34aa03019baed
jhhwang <iskasion@naver.com>
2023-09-26 09:44:19 +0900

    init

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+
+app = Flask(__name__)  # 파일 이름과 같은 Flask 앱 객체를 만듭니다
+
+@app.route("/")  # "/" 경로로 들어오면 이 함수를 마주칩니다
+def hello():
+    return "Hellow, World!"  # "/" 경로로 들어오면 "Hello, World!"를 출력합니다
